f59bfe25d58ca0c956b6798bac172688e3fcd20e
neverparadise <ye20013@gmail.com>
2021-06-11 17:08:15 +0900

    Update branch

--- utils.py
+++ utils.py
@@ -228,6 +228,41 @@ def make_action(env, action_index):
 
     return action
 
+
+def make_action2(env, action_index):
+    # Action들을 정의
+    action = env.action_space.noop()
+    if (action_index == 0):
+        action['camera'] = [0, -5]
+        action['attack'] = 1
+    elif (action_index == 1):
+        action['camera'] = [0, 5]
+        action['attack'] = 1
+    elif (action_index == 2):
+        action['camera'] = [-5, 0]
+        action['attack'] = 1
+    elif (action_index == 3):
+        action['camera'] = [5, 0]
+        action['attack'] = 1
+    elif (action_index == 4):
+        action['forward'] = 1
+        action['jump'] = 1
+    elif (action_index == 5):
+        action['forward'] = 1
+        action['attack'] = 1
+    elif (action_index == 6):
+        action['back'] = 1
+        action['attack'] = 1
+    elif (action_index == 7):
+        action['left'] = 1
+        action['attack'] = 1
+    elif (action_index == 8):
+        action['right'] = 1
+        action['attack'] = 1
+
+    return action
+
+
 def converter(observation):
         obs = observation['pov']
         obs = obs / 255.0
